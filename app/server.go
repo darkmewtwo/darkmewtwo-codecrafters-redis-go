45e1bc6e63625ecf88da97c6787283dd443bcf50
darkmewtwo <ashleysunil07@gmail.com>
2024-04-01 15:43:47 +0530

    added command for echo

--- app/server.go
+++ app/server.go
@@ -23,7 +23,10 @@ func handleConnection(conn net.Conn) {
 		cmd := strings.TrimSpace(request)
 		cmd_parts := strings.Split(cmd, "\\r\\n")
 		fmt.Println(cmd, cmd_parts)
-		keyword := strings.ToLower(cmd_parts[2])
+		keyword := ""
+		if len(cmd_parts) > 1 {
+			keyword = strings.ToLower(cmd_parts[2])
+		}
 		switch keyword {
 
 		case "ping":
