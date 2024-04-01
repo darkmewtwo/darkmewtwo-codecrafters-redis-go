d94e0b2a6104138fbe5cf95860df019fb91245b7
darkmewtwo <ashleysunil07@gmail.com>
2024-04-01 23:39:17 +0530

    set n get test

--- app/server.go
+++ app/server.go
@@ -10,6 +10,42 @@ import (
 	"os"
 )
 
+const (
+	CRLF             = "\r\n"
+	SIMPLE_STRING    = "+"
+	SIMPLE_ERROR     = "-"
+	INTEGER          = ":"
+	BULK_STRINGS     = "$"
+	ARRAY            = "*"
+	BULK_NULL_STRING = "-1"
+)
+
+var dataStore map[string]string
+
+func init() {
+	dataStore = make(map[string]string)
+}
+
+func constructResponseMessage(resp_data_type string, data string) string {
+
+	message := resp_data_type + data + CRLF
+	return message
+}
+
+func setData(key string, value string) {
+	dataStore[key] = value
+	// return true
+}
+
+func getData(key string) (string, string) {
+	value, exists := dataStore[key]
+	if exists {
+		return SIMPLE_STRING, value
+	} else {
+		return BULK_STRINGS, BULK_NULL_STRING
+	}
+}
+
 func handleConnection(conn net.Conn) {
 	// defer conn.Close()
 	// fmt.Println(conn)
@@ -18,7 +54,7 @@ func handleConnection(conn net.Conn) {
 		buffer := make([]byte, 1024)
 		buffN, _ := conn.Read(buffer)
 
-		// fmt.Println(buffN)
+		// fmt.Println(buffer)
 		request := string(buffer[:buffN])
 		fmt.Println(strings.Fields(request), request, "M")
 		// cmd := strings.TrimSpace(request)
@@ -30,14 +66,19 @@ func handleConnection(conn net.Conn) {
 		if len(cmd_parts) > 1 {
 			keyword = strings.ToLower(cmd_parts[2])
 		}
-		fmt.Println(keyword)
+		// fmt.Println(keyword)
 		switch keyword {
 
 		case "ping":
-			conn.Write([]byte("+PONG\r\n"))
+			conn.Write([]byte(constructResponseMessage(SIMPLE_STRING, "PONG")))
 		case "echo":
-			message := "+" + cmd_parts[4] + "\r\n"
-			conn.Write([]byte(message))
+			conn.Write([]byte(constructResponseMessage(SIMPLE_STRING, cmd_parts[4])))
+		case "set":
+			setData(cmd_parts[4], cmd_parts[6])
+			conn.Write([]byte(constructResponseMessage(SIMPLE_STRING, "OK")))
+		case "get":
+			dataType, value := getData(cmd_parts[4])
+			conn.Write([]byte(constructResponseMessage(dataType, value)))
 		default:
 			conn.Write([]byte("+PONG\r\n"))
 		}
